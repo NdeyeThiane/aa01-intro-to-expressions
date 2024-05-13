37d3719b95d98490292ab57082a451a5980ae972
Ndeye <ndeyet2004@gmail.com>
2024-05-13 14:35:47 -0700

    I finish the homework but problem 02 is not passing even though I did it correctly

--- problems/02-code-comments.js
+++ problems/02-code-comments.js
@@ -8,16 +8,37 @@ file.
 console.log('Welcome to App Academy!');
 
 /* Print more messages to the terminal using console.log */
+//console.log("What's your name? ");
+//console.log("how old are you?");
+console.log("nice to meet you!");
+
+
 
 // 2. This is a single line comment.
-// console.log('This is will not be printed to the terminal')
+console.log('This is will not be printed to the terminal')
 // Try adding your own single line comments:
 
-// Your code here 
+//added a single-line comment
+
+
+// Your code here
+
+//added a single-line comment
+
+//Example of single comment I can only comment on one line.
+
 
 /* 3.
 This is a multi-line comment.
 Try adding your own multi-line comments:
 */
 
-// Your code here 
+// Your code here
+
+
+
+/* Hi, trying
+a multi line comment
+I can write as much I
+I want here
+it will ignore it. */
